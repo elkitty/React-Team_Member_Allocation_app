ad337035b6eaed6d65191cbd4a00c48909759f75
elkitty <hablaconwafle@gmail.com>
2023-03-06 19:47:50 -0300

    fixed bugs in Header and improves code in Nav

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ const Header = ({ selectedTeam, teamMemberCount }) => {
         <div className="col-8">
           <h1 className="contan i">Team Member Allocation</h1>
           <h3>
-            {selectedTeam} has {teamMemberCount} members
+            {selectedTeam} has {teamMemberCount} {teamMemberCount <= 1 ? "member": "members"}
           </h3>
         </div>
       </div>
--- src/Nav.js
+++ src/Nav.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
   return (
@@ -7,11 +7,12 @@ const Nav = () => {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <Link className="nav-link" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
           <Link className="nav-link" to="/GroupedTeamMembers">Teams</Link>
         </li>
       </ul>
     </nav>
-  );
-};
-
+  )
+}
 export default Nav
